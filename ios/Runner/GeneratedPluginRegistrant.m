115993d1afd42840b00995168f78faecb5f1271e
船長 <erichsiao@chuanzhangde-MacBook-Pro.local>
2020-01-07 10:38:15 +0800

    adjust

--- ios/Runner/GeneratedPluginRegistrant.m
+++ ios/Runner/GeneratedPluginRegistrant.m
@@ -3,10 +3,30 @@
 //
 
 #import "GeneratedPluginRegistrant.h"
+
+#if __has_include(<fluttertoast/FluttertoastPlugin.h>)
 #import <fluttertoast/FluttertoastPlugin.h>
-#import <package_info/PackageInfoPlugin.h>
-#import <path_provider/PathProviderPlugin.h>
+#else
+@import fluttertoast;
+#endif
+
+#if __has_include(<package_info/FLTPackageInfoPlugin.h>)
+#import <package_info/FLTPackageInfoPlugin.h>
+#else
+@import package_info;
+#endif
+
+#if __has_include(<path_provider/FLTPathProviderPlugin.h>)
+#import <path_provider/FLTPathProviderPlugin.h>
+#else
+@import path_provider;
+#endif
+
+#if __has_include(<sqflite/SqflitePlugin.h>)
 #import <sqflite/SqflitePlugin.h>
+#else
+@import sqflite;
+#endif
 
 @implementation GeneratedPluginRegistrant
 
